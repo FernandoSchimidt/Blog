737cc52bd0f72ca09e761180fa1655f2e0fe6179
fernando <fernandoschimidt@outlook.com>
2019-10-31 15:24:00 -0300

    Middleware

--- app.js
+++ app.js
@@ -5,21 +5,47 @@ const bodyParser = require("body-parser")
 const app = express()
 const admin = require("./routes/admin")
 const path = require("path")
-    //const mongoose = require("mongose")
+const mongoose = require("mongoose")
+const session = require("express-session")
+const flash = require("connect-flash")
 
 //Configurações
-//Body PArser
+//sessao
+app.use(session({
+    secret: "cursodenode",
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(flash())
+
+//Midleware
+app.use((req, res, next) => {
+        res.locals.success_msg = req.flash("seccess_msg")
+        res.locals.error_msg = req.flash("error_msg")
+        next()
+    })
+    //Body PArser
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
     //Handlebars
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars');
 //Monogoose
-
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/blogapp').then(() => {
+    console.log("Conectado ao mongo!")
+}).catch((erro) => {
+    console.log("Erro" + erro)
+})
 
 //Public
 app.use(express.static(path.join(__dirname, "public")))
 
+
+app.use((req, res, next) => {
+    console.log("Oi eu sou um midleware")
+    next()
+})
 
 //
 //sempre chamar as rotas abaixo das configurações
